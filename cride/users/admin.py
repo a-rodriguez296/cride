1438e40d21a6dc2c449fa52b63928df7b277c7e0
Alejandro Rodríguez <a.rodriguez@globant.com>
2020-01-23 14:11:53 -0500

    Agregando user y profile al admin

---
+++ cride/users/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+
+from cride.users.models import User, Profile
+
+class CustomUserAdmin(UserAdmin):
+
+    list_display = ('email', 'username', 'first_name', 'last_name', 'is_staff', 'is_client')
+    list_filter = ('is_client', 'is_staff', 'created', 'modified')
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+
+    list_display = ('user', 'reputation', 'rides_taken', 'rides_offered')
+    search_fields = ('user__username', 'user_email', 'user_first_name', 'user_last_name')
+    list_filter = ('reputation',)
+
+admin.site.register(User, CustomUserAdmin)
+
+
