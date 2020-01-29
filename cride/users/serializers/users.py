55cfaabfc60193a6bcfe5f549362dbc28bd7ca56
Alejandro Rodríguez <a.rodriguez@globant.com>
2020-01-29 18:32:18 -0500

    Utilizando la librería de django para enviar correos en dev. Realmente no se envia un correo como tal. Lo que se hace es imprimir en consola como quedaría

--- cride/users/serializers/users.py
+++ cride/users/serializers/users.py
@@ -1,8 +1,12 @@
 from django.contrib.auth import authenticate, password_validation
+from django.core.mail import EmailMultiAlternatives
+from django.core.validators import RegexValidator
+from django.template.loader import render_to_string
+
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token 
 from rest_framework.validators import UniqueValidator
-from django.core.validators import RegexValidator
+
 
 from cride.users.models import User, Profile
 
@@ -59,7 +63,30 @@ class UserSignupSerializer(serializers.Serializer):
         data.pop('password_confirmation')
         user = User.objects.create_user(**data, is_verified=False)
         profile = Profile.objects.create(user=user)
+        self.send_confirmation_email(user)
         return user
+
+    def send_confirmation_email(self, user):
+        #SEnd a verification mail to user and generate unique token
+        verification_token = self.gen_verification_token(user)
+        subject = "Welcome @{}! Verify your account to start using Comparte Rides".format(user.username)
+        from_email = 'Comparte Ride <noreply@comparteride.com'
+        content = render_to_string(
+            'emails/users/account_verification.html',
+            {
+                'token': verification_token,
+                'user': user
+            }
+            )
+        msg = EmailMultiAlternatives(subject, content, from_email, [user.email])
+        msg.attach_alternative(content, "text/html")
+        msg.send()
+        
+
+    def gen_verification_token(self, user):
+        #Create JWT token that the user can use to verify account. 
+        return 'abc'
+
 
 
 class UserLoginSerializer(serializers.Serializer):
