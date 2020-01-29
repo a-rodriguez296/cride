8c5adb2d90658a1d053d9609525341b6c96984cd
Alejandro Rodríguez <a.rodriguez@globant.com>
2020-01-29 00:27:56 -0500

    Haciendo el endpoint del login

---
+++ cride/users/serializers/users.py
@@ -0,0 +1,15 @@
+from django.contrib.auth import authenticate
+from rest_framework import serializers
+
+class UserLoginSerializer(serializers.Serializer):
+    
+    email = serializers.EmailField()
+    password = serializers.CharField(min_length=8, max_length=64)
+
+    def validate(self, data):
+        user = authenticate(username=data['email'], password=data['password'])
+        if not user:
+            raise serializers.ValidationError('Invalid credentials')
+        return data
+
+
